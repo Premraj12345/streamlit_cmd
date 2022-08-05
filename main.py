de077845f04314b87a5dd1e20cc31835fff74dc8
Premraj12345 <94243441+Premraj12345@users.noreply.github.com>
2022-08-05 17:34:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import streamlit as st
 import os
+from download import download
 
 st.header('CMD')
+button_click2 = st.buttton('Download the zip File') 
 
 cmd_input = st.text_input('Enter a command')
 button_click = st.button('RUN')
+
 
 if button_click == True:
     with st.container():
         text = os.popen(cmd_input).read()
         st.markdown(text)
+if button_click2 == True:
+    with st.container():
+        status = download('https://github.com/dreamnettech/dreampower-checkpoints/releases/download/0.0.1/v0.0.1.zip','model.zip')
+        st.markdown(status)
