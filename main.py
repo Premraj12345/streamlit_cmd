19fe7130de87731560219b3ddd2d4799bbb68794
Premraj12345 <94243441+Premraj12345@users.noreply.github.com>
2022-08-05 14:20:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ cmd_input = st.text_input('Enter a command')
 button_click = st.button('RUN')
 
 if button_click == True:
-    with st.container()
+    with st.container():
         text = os.popen(cmd_input).read()
         st.markdown(text)
