805f19cb7136c6151cc6dd59d5fa6ddf72fea904
Premraj12345 <94243441+Premraj12345@users.noreply.github.com>
2022-08-05 18:05:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ button_click2 = st.button('Download the zip File')
 
 cmd_input = st.text_input('Enter a command')
 button_click = st.button('RUN')
-
+button_click3 = st.button('Extract the Model.zip File')
 
 if button_click == True:
     with st.container():
@@ -18,7 +18,7 @@ if button_click2 == True:
     with st.container():
         status = download('https://github.com/dreamnettech/dreampower-checkpoints/releases/download/0.0.1/v0.0.1.zip','model.zip')
         st.markdown(status)
-button_click3 = st.button(f'Extract the {status} File')
+
 if button_click3 == True:
     with ZipFile('model.zip','r') as zipObj:
         zipObj.extractall()
