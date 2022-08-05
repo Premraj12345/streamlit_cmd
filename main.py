60b68b0c7bffc694c1138bb4139f8088bf39cf77
Premraj12345 <94243441+Premraj12345@users.noreply.github.com>
2022-08-05 17:58:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import os
 from download import download
+from zipfile import ZipFile
 
 st.header('CMD')
 button_click2 = st.button('Download the zip File') 
@@ -17,3 +18,7 @@ if button_click2 == True:
     with st.container():
         status = download('https://github.com/dreamnettech/dreampower-checkpoints/releases/download/0.0.1/v0.0.1.zip','model.zip')
         st.markdown(status)
+        button_click3 = st.button(f'Extract the {status} File')
+        if button_click3 == True:
+            with ZipFile('model.zip','r') as zipObj:
+                zipObj.extractall()
